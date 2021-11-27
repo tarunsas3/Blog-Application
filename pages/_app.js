34ca2148c47c9b35cc97587555ee2f9779d54b01
Tarun <tarunsastry99@gmail.com>
2021-11-28 00:22:51 +0530

    Initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import 'tailwindcss/tailwind.css'
+import React from "react";
+import "../styles/globals.scss";
+import { Layout } from "../components";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
