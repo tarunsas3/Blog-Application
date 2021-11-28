b945c689a9de9fcf5d9594f8138e7401dabd991c
Tarun Sastry <60286295+tarunsas3@users.noreply.github.com>
2021-11-28 12:31:32 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Graph CMS
+              GraphLog
             </span>
           </Link>
         </div>
